1f7ede31022907f0d58ffdb26fbc071f1a03f246
CDDelta <ngjunyen@gmail.com>
2020-05-15 20:59:11 +1000

    remove unused components

--- projects/ngx-monetization/src/lib/monetization.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { NgxMonetizationComponent } from './monetization.component';
-
-@NgModule({
-  declarations: [NgxMonetizationComponent],
-  imports: [],
-  exports: [NgxMonetizationComponent],
-})
-export class MonetizationModule {}
