5432649a8e01296ec4b008cfad91f3296388d214
CDDelta <ngjunyen@gmail.com>
2020-05-16 10:24:40 +1000

    update ng add schematic

--- projects/ngx-monetization/schematics/ng-add/index.ts
+++ projects/ngx-monetization/schematics/ng-add/index.ts
@@ -1,9 +1,42 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import {
+  Rule,
+  SchematicContext,
+  SchematicsException,
+  Tree,
+} from '@angular-devkit/schematics';
 import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+import {
+  appendHtmlElementToHead,
+  getProjectFromWorkspace,
+  getProjectIndexFiles,
+} from '@angular/cdk/schematics';
+import { getWorkspace } from '@schematics/angular/utility/config';
+import { Schema } from './schema';
 
-export function ngAdd(_options: any): Rule {
+export function ngAdd(options: Schema): Rule {
   return (tree: Tree, _context: SchematicContext) => {
+    // Add payment pointer to index.html.
+    const workspace = getWorkspace(tree);
+    const project = getProjectFromWorkspace(workspace, options.project);
+    const projectIndexFiles = getProjectIndexFiles(project);
+
+    if (!projectIndexFiles.length) {
+      throw new SchematicsException(
+        'No project index HTML file could be found.'
+      );
+    }
+
+    projectIndexFiles.forEach((indexFilePath) => {
+      appendHtmlElementToHead(
+        tree,
+        indexFilePath,
+        `<meta name="monetization" content="${options.paymentPointer}">`
+      );
+    });
+
+    // Install the package.
     _context.addTask(new NodePackageInstallTask());
+
     return tree;
   };
 }
