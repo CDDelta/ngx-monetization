cdd47dd47784b00bb31ffc3754a18d91bdd15dff
CDDelta <ngjunyen@gmail.com>
2020-05-16 12:30:05 +1000

    update demo functionality

--- projects/e2e-app/src/app/app.component.ts
+++ projects/e2e-app/src/app/app.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
 import { MonetizationService, MonetizationEvent } from 'ngx-monetization';
 import { Observable } from 'rxjs';
-import { scan } from 'rxjs/operators';
+import { scan, filter, startWith } from 'rxjs/operators';
+import { MonetizationProgressEvent } from 'types-wm';
+
+export interface PaidTotal {
+  amount: number;
+  formattedAmount: string;
+  currency: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -10,15 +17,44 @@ import { scan } from 'rxjs/operators';
 })
 export class AppComponent {
   public events$: Observable<MonetizationEvent[]>;
+  public paidTotal$: Observable<PaidTotal>;
 
   constructor(public monetization: MonetizationService) {
-    monetization.setPaymentPointer("$wallet.example.com/alice");
+    monetization.setPaymentPointer('$wallet.example.com/alice');
 
     this.events$ = monetization.events.pipe(
-      scan((acc, val) => {
-        acc.push(val);
-        return acc.slice(-3);
-      }, [])
+      scan((eventsList, e) => {
+        // Unroll the event manually as JSON.stringify() will not properly serialise the event.
+        eventsList.push({
+          type: e.type,
+          detail: {
+            ...e.detail,
+          },
+        });
+        return eventsList.slice(-10);
+      }, []),
+      startWith([])
+    );
+
+    const startTotal = {
+      amount: 0,
+      formattedAmount: '',
+      currency: '',
+    };
+
+    this.paidTotal$ = monetization.events.pipe(
+      filter((e) => e.type === 'monetizationprogress'),
+      scan((total, e: MonetizationProgressEvent) => {
+        const scale = e.detail.assetScale;
+        const amount = total.amount + Number(e.detail.amount);
+
+        return {
+          amount,
+          formattedAmount: (amount * Math.pow(10, -scale)).toFixed(scale),
+          currency: e.detail.assetCode,
+        };
+      }, startTotal),
+      startWith(startTotal)
     );
   }
 }
